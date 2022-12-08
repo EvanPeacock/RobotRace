5fd6f7a01be13b9b2e7e04bb798c17b36a543cff
Evan Peacock <evan.james.peacock@gmail.com>
2022-12-08 15:00:31 -0500

    Added Julia's Code

--- RobotRace.py
+++ RobotRace.py
@@ -1,3 +1,4 @@
+import random
 from spike import Motor, DistanceSensor, PrimeHub, MotorPair, ColorSensor
 from spike.control import wait_for_seconds, Timer
 
@@ -11,12 +12,26 @@ motors = MotorPair('C', 'D')
 col_sensor_A = ColorSensor('A')
 col_sensor_B = ColorSensor('B')
 
+
+def randomAction():
+    num = random.randint(0,2) # However many different actions we want!
+    match num:
+        case 0: # Spin in circle
+            motors.stop()
+            wait_for_seconds(2)
+        case 1: # speed up for 2 seconds
+            motors.start(75)
+            wait_for_seconds(2)
+            motors.start()
+        case 2: # slow down for 3 seconds
+            motors.start(25)
+            wait_for_seconds(2)
+            motors.start()
+
 def race():
-    running = False
     #Press the left button to start
     while True:
         if hub.left_button.was_pressed():
-            running = True
             hub.speaker.beep(60, 0.3)
             wait_for_seconds(0.8)
             hub.speaker.beep(60, 0.3)
@@ -40,16 +55,15 @@ def race():
 
         #Powerup detection
         if col_sensor_A.get_color() == 'green':
-            hub.light_matrix.show_image('DIAMOND')
             print("Powerup Detected")
             hub.speaker.beep(55, 0.2)
             hub.speaker.beep(65, 0.3)
             hub.speaker.beep(55, 0.2)
+            randomAction()
             hub.light_matrix.show_image('ARROW_N')
 
     #Stop the robot when the finish line is detected        
     motors.stop()
-    running = False
     hub.light_matrix.show_image('CHESSBOARD')
     print("Total Time: " + str(timer.now()) + " seconds")
     hub.speaker.beep(60, 0.2)
