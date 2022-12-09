fbab7f011aed5f645b9bceb7546242cc5cdfe612
Evan Peacock <evan.james.peacock@gmail.com>
2022-12-08 19:29:10 -0500

    Added Evan Medium Part

--- RobotRace.py
+++ RobotRace.py
@@ -1,6 +1,6 @@
-import random
 from spike import Motor, DistanceSensor, PrimeHub, MotorPair, ColorSensor
 from spike.control import wait_for_seconds, Timer
+import random
 
 # Initialize the Hub
 hub = PrimeHub()
@@ -13,20 +13,112 @@ col_sensor_A = ColorSensor('A')
 col_sensor_B = ColorSensor('B')
 
 
+def matrixSpin():
+    hub.light_matrix.off()
+    hub.light_matrix.set_pixel(1, 0)
+    hub.light_matrix.set_pixel(0, 1)
+    hub.light_matrix.set_pixel(0, 2)
+    hub.light_matrix.set_pixel(0, 3)
+    hub.light_matrix.set_pixel(1, 4)
+    hub.light_matrix.set_pixel(2, 4)
+    hub.light_matrix.set_pixel(3, 4)
+    hub.light_matrix.set_pixel(4, 2)
+    hub.light_matrix.set_pixel(4, 3)
+    hub.light_matrix.set_pixel(3, 1)
+    hub.light_matrix.set_pixel(2, 2)
+    hub.speaker.beep(75, 0.2)
+    hub.speaker.beep(60, 0.2)
+    hub.speaker.beep(75, 0.2)
+    hub.speaker.beep(60, 0.2)
+
+def matrixSpeedUpTemp():
+    hub.light_matrix.off()
+    hub.light_matrix.set_pixel(0, 2)
+    hub.light_matrix.set_pixel(1, 1)
+    hub.light_matrix.set_pixel(2, 0)
+    hub.light_matrix.set_pixel(3, 1)
+    hub.light_matrix.set_pixel(4, 2)
+
+    hub.light_matrix.set_pixel(1, 4)
+    hub.light_matrix.set_pixel(2, 3)
+    hub.light_matrix.set_pixel(3, 4)
+    hub.speaker.beep(60, 0.3)
+    hub.speaker.beep(70, 0.3)
+    hub.speaker.beep(80, 0.3)
+    hub.speaker.beep(90, 0.3)
+
+def matrixSlowDownTemp():
+    hub.light_matrix.off()
+    hub.light_matrix.set_pixel(0, 2)
+    hub.light_matrix.set_pixel(1, 3)
+    hub.light_matrix.set_pixel(2, 4)
+    hub.light_matrix.set_pixel(3, 3)
+    hub.light_matrix.set_pixel(4, 2)
+
+    hub.light_matrix.set_pixel(1, 0)
+    hub.light_matrix.set_pixel(2, 1)
+    hub.light_matrix.set_pixel(3, 0)
+    hub.speaker.beep(90, 0.3)
+    hub.speaker.beep(80, 0.3)
+    hub.speaker.beep(70, 0.3)
+    hub.speaker.beep(60, 0.3)
+
+def matrixSpeedUpPerm():
+    hub.light_matrix.off()
+    hub.light_matrix.set_pixel(0, 2)
+    hub.light_matrix.set_pixel(1, 1)
+    hub.light_matrix.set_pixel(2, 0)
+    hub.light_matrix.set_pixel(3, 1)
+    hub.light_matrix.set_pixel(4, 2)
+    hub.speaker.beep(60, 0.3)
+    hub.speaker.beep(75, 0.3)
+    hub.speaker.beep(90, 0.3)
+
+def matrixSlowDownPerm():
+    hub.light_matrix.off()
+    hub.light_matrix.set_pixel(0, 2)
+    hub.light_matrix.set_pixel(1, 3)
+    hub.light_matrix.set_pixel(2, 4)
+    hub.light_matrix.set_pixel(3, 3)
+    hub.light_matrix.set_pixel(4, 2)
+    hub.speaker.beep(90, 0.3)
+    hub.speaker.beep(75, 0.3)
+    hub.speaker.beep(60, 0.3)
+
 def randomAction():
-    num = random.randint(0,2) # However many different actions we want!
-    match num:
-        case 0: # Spin in circle
-            motors.stop()
-            wait_for_seconds(2)
-        case 1: # speed up for 2 seconds
-            motors.start(75)
-            wait_for_seconds(2)
-            motors.start()
-        case 2: # slow down for 3 seconds
-            motors.start(25)
-            wait_for_seconds(2)
-            motors.start()
+    num = random.randint(0, 4) # However many different actions we want!
+
+    if num == 0:
+        #Spin in place
+        print("Spin")
+        matrixSpin()
+        motors.start(20, -20)
+        wait_for_seconds(2)
+        motors.start()
+    elif num == 1:
+        #Speed up for 2 seconds
+        print("Temporary Speed Up")
+        matrixSpeedUpTemp()
+        motors.start(30)
+        wait_for_seconds(2)
+        motors.start()
+    elif num == 2:
+        #Slow down for 2 seconds
+        print("Temporary Slow Down")
+        matrixSlowDownTemp()
+        motors.start(10)
+        wait_for_seconds(2)
+        motors.start()
+    elif num == 3:
+        #Speed up permanently
+        print("Permanent Speed Up")
+        matrixSpeedUpPerm()
+        motors.start(30)
+    elif num == 4:
+        #Slow down permanently
+        print("Permanent Slow Down")
+        matrixSlowDownPerm()
+        motors.start(10)
 
 def race():
     #Press the left button to start
@@ -43,26 +135,23 @@ def race():
             timer = Timer()
             print("Timer Started")
             break
-            
+
     while(col_sensor_A.get_color() != 'red'):
         #Line following
         if col_sensor_A.get_color() == 'black':
             motors.start(20, 15)
         elif col_sensor_B.get_color() == 'black':
-            motors.start(-15) 
+            motors.start(-15)
         else:
             motors.start()
 
         #Powerup detection
         if col_sensor_A.get_color() == 'green':
             print("Powerup Detected")
-            hub.speaker.beep(55, 0.2)
-            hub.speaker.beep(65, 0.3)
-            hub.speaker.beep(55, 0.2)
             randomAction()
             hub.light_matrix.show_image('ARROW_N')
 
-    #Stop the robot when the finish line is detected        
+    #Stop the robot when the finish line is detected
     motors.stop()
     hub.light_matrix.show_image('CHESSBOARD')
     print("Total Time: " + str(timer.now()) + " seconds")
@@ -70,7 +159,5 @@ def race():
     hub.speaker.beep(65, 0.3)
     hub.speaker.beep(70, 0.2)
     hub.speaker.beep(75, 0.3)
-
-
 
 race()
